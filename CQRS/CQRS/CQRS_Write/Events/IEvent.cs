d9e02718038d83b75687b7045575d1aca41df9f0
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2021-11-21 22:06:46 -0300

    Adicionando camada de Write

---
+++ CQRS/CQRS/CQRS_Write/Events/IEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Write.Events
+{
+    public interface IEvent
+    {
+        long TimeStamp { get; set; }
+        int AggregateId { get; set; }
+        string Type { get; }
+        int Version { get; set; }
+    }
+}
---
+++ CQRS/CQRS/CQRS_Write/Events/IEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Write.Events
+{
+    public interface IEventPublisher
+    {
+        void RegisterEventHandlers(IEventHandler eventHandler);
+        void Publish<T>(T @evento) where T : IEvent;
+    }
+}
