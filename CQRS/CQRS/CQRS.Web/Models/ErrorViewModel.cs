807802ef3311d741dd565e884f8c1cdd2fc98020
Felipe Mattioli <ext.felipe.mattioli@farfetch.com>
2022-03-13 11:53:42 -0300

    Implemenando CQRS com MediatR

--- CQRS/CQRS/CQRS.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CQRS.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
